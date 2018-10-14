7d2992c6b439efce74039e69f3261ea9456e062c
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-14 08:11:11 +0500

    Update Added New student Controller

---
+++ QuizDemoApplication/Controllers/StudentsController.cs
@@ -0,0 +1,49 @@
+﻿using QuizDemoApplication.Models;
+using QuizDemoClasses;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace QuizDemoApplication.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private QuizDemoContext _db = new QuizDemoContext();
+        // GET: Students
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult StudentRegistration()
+        {
+
+            ViewBag.Types = _db.Tests.Where(x => x.IsActive == true).Select(x => new { x.Id, x.Name }).ToList();
+            SessionModel model = null;
+            if (Session["SessionModel"] == null)
+                model = new SessionModel();
+
+            else
+                model = (SessionModel)Session["SessionModel"];
+
+
+            return View(model);
+        }
+
+        public ActionResult Instruction(SessionModel sessionModel)
+        {
+            if (sessionModel != null)
+            {
+                var test = _db.Tests.Where(c => c.IsActive == true && c.Id == sessionModel.TestId).FirstOrDefault();
+                if (test != null)
+                {
+                    ViewBag.TestName = test.Name;
+                    ViewBag.TestDescription = test.Description;
+                    ViewBag.QuestionCount = test.TestQuestion;
+                    ViewBag.TestDuration = test.DurationInMint;
+
+                }
+            }
+            return View(sessionModel);
+
+        }
+    }
+}
