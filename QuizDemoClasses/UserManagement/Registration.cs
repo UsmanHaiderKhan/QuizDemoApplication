8a92bea11b07708c35d1d9f3a3090d701c5bc154
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-14 08:21:03 +0500

    Update the Association in the registration class

---
+++ QuizDemoClasses/UserManagement/Registration.cs
@@ -0,0 +1,20 @@
+﻿using QuizDemoClasses.QuizManagement;
+using System;
+
+namespace QuizDemoClasses.UserManagement
+{
+    public class Registration
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public int TestId { get; set; }
+        public DateTime RegistrationDate { get; set; }
+        public string Token { get; set; }
+        public DateTime TokenExpireTime { get; set; }
+        public Student Student { get; set; }
+        public TestPaper TestPaper { get; set; }
+        public Test Test { get; set; }
+        public QuestionDuration QuestionDuration { get; set; }
+
+    }
+}
