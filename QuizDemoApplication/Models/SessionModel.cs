c111d340a1e62dd0fd3205a1556cfdfa0334d778
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-14 08:11:45 +0500

    Update Added a Model Class to Maintain the Session of the Student

---
+++ QuizDemoApplication/Models/SessionModel.cs
@@ -0,0 +1,11 @@
+﻿namespace QuizDemoApplication.Models
+{
+    public class SessionModel
+    {
+        public int TestId { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PhonNo { get; set; }
+
+    }
+}
