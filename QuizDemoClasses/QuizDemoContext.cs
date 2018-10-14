58d289878e316574ce3b164762148c55c198971a
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-14 08:18:02 +0500

    Update  added New Context class of the database

---
+++ QuizDemoClasses/QuizDemoContext.cs
@@ -0,0 +1,26 @@
+﻿using QuizDemoClasses.QuizManagement;
+using QuizDemoClasses.UserManagement;
+using System.Data.Entity;
+
+namespace QuizDemoClasses
+{
+    public class QuizDemoContext : DbContext
+    {
+        public QuizDemoContext() : base("name=constr")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Registration> Registrations { get; set; }
+        public DbSet<Choice> Choices { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<TestQuestion> TestQuestions { get; set; }
+
+        public DbSet<TestPaper> TestPapers { get; set; }
+        public DbSet<Test> Tests { get; set; }
+        public DbSet<QuestionCategory> QuestionCategories { get; set; }
+        public DbSet<QuestionDuration> QuestionDurations { get; set; }
+
+    }
+}
