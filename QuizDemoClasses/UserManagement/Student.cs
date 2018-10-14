fa5ae11c31f7fea69466d12e72e260ebde25dff0
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-14 08:21:32 +0500

    Update the student class

---
+++ QuizDemoClasses/UserManagement/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace QuizDemoClasses.UserManagement
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AccessLevel { get; set; }
+        public DateTime EntryDate { get; set; }
+        public string Email { get; set; }
+        public long PhonNo { get; set; }
+        public int PassHash { get; set; }
+
+    }
+}
