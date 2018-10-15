eb7c2eb25a1390241bd10b0bfc6c7e356458e58b
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-15 19:40:13 +0500

    Update the Registration Association added List of the studentd

--- QuizDemoClasses/UserManagement/Registration.cs
+++ QuizDemoClasses/UserManagement/Registration.cs
@@ -9,9 +9,9 @@ namespace QuizDemoClasses.UserManagement
         public int StudentId { get; set; }
         public int TestId { get; set; }
         public DateTime RegistrationDate { get; set; }
-        public string Token { get; set; }
+        public Guid Token { get; set; }
         public DateTime TokenExpireTime { get; set; }
-        public Student Student { get; set; }
+
         public TestPaper TestPaper { get; set; }
         public Test Test { get; set; }
         public QuestionDuration QuestionDuration { get; set; }
