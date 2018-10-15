f6006c30a6b3d476c1b1d68b5112cff063ee414a
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-15 19:02:58 +0500

    Update added QuestionModel class

---
+++ QuizDemoApplication/Models/QuestionModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace QuizDemoApplication.Models
+{
+    public class QuestionModel
+    {
+        public int TotalQuestionInSet { get; set; }
+        public int QuestionNumber { get; set; }
+        public int TestId { get; set; }
+        public string TestName { get; set; }
+        public string Question { get; set; }
+        public string QuestionType { get; set; }
+        public int Point { get; set; }
+        public List<OptionsModel> Options { get; set; }
+    }
+}
