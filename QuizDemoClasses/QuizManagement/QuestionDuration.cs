a7bda69715334b2db1771395d78ef3e8dac197cf
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-16 07:47:18 +0500

    Added Class to Store the Duration of each quiz

---
+++ QuizDemoClasses/QuizManagement/QuestionDuration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace QuizDemoClasses.QuizManagement
+{
+    public class QuestionDuration
+    {
+        public int Id { get; set; }
+        public int RegistrationId { get; set; }
+        public int TestQuestionId { get; set; }
+        public DateTime RequesTime { get; set; }
+        public DateTime LeaveTime { get; set; }
+        public DateTime AnswerTime { get; set; }
+
+
+
+
+    }
+}
