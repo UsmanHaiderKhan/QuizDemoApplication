8d8cd78e15d3885d65a5943c2fdd7ad0f6e248eb
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-16 07:49:14 +0500

    UPdate Added Test Question Class in the Project

---
+++ QuizDemoClasses/QuizManagement/TestQuestion.cs
@@ -0,0 +1,20 @@
+﻿namespace QuizDemoClasses.QuizManagement
+{
+    public class TestQuestion
+    {
+
+        public int Id { get; set; }
+        public int TestId { get; set; }
+        public int QuestionId { get; set; }
+        public int QuestionNumber { get; set; }
+        public bool IsActive { get; set; }
+        public Question Question { get; set; }
+        public Test Test { get; set; }
+        public QuestionDuration QuestionDuration { get; set; }
+        public TestPaper TestPaper { get; set; }
+
+
+
+
+    }
+}
