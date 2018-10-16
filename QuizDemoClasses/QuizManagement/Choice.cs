3445a31807c1165fd7cafdc0c417b51d92c3b2d2
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-16 07:45:56 +0500

    Update the Choice class

---
+++ QuizDemoClasses/QuizManagement/Choice.cs
@@ -0,0 +1,14 @@
+﻿namespace QuizDemoClasses.QuizManagement
+{
+    public class Choice
+    {
+        public int Id { get; set; }
+        public int QuestionId { get; set; }
+        public string Lable { get; set; }
+        public int Points { get; set; }
+        public bool IsActive { get; set; }
+
+
+
+    }
+}
