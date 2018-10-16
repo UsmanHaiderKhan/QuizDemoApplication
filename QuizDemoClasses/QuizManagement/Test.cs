210fa8be13fd82ad531c6540d7cffce87f9e077c
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-16 07:48:01 +0500

    Added Test class to store a compelete Quiz information

---
+++ QuizDemoClasses/QuizManagement/Test.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace QuizDemoClasses.QuizManagement
+{
+    public class Test
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+
+        public double DurationInMint { get; set; }
+
+        public List<TestQuestion> TestQuestions { get; set; }
+
+        public Test()
+        {
+            TestQuestions = new List<TestQuestion>();
+        }
+
+
+    }
+
+}
