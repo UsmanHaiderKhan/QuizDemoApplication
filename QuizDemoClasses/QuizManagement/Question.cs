74cbf7f221dfa7d0fddaacca3d69a5cf928b064c
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-16 07:46:19 +0500

    Added New Question class to store the question

---
+++ QuizDemoClasses/QuizManagement/Question.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace QuizDemoClasses.QuizManagement
+{
+    public class Question
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public string QuestionType { get; set; }
+        public string Questino1 { get; set; }
+        public int Points { get; set; }
+        public bool IsActive { get; set; }
+        public QuestionCategory QuestionCategory { get; set; }
+
+        public List<Choice> Choices { get; set; }
+
+        public Question()
+        {
+            Choices = new List<Choice>();
+        }
+
+    }
+
+
+}
