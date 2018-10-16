cb7f2c1912509e7f1967aaf2e11d5372a1fb7816
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-16 07:46:46 +0500

    Added class to store the question CAtegory

---
+++ QuizDemoClasses/QuizManagement/QuestionCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace QuizDemoClasses.QuizManagement
+{
+    public class QuestionCategory
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+
+
+    }
+}
