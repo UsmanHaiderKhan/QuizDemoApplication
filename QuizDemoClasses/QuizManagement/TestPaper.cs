01b1dcdda59a084b29469f4b9db63bc9d2c1674c
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-16 07:48:38 +0500

    Added New Test Paper class in the  Database tables

---
+++ QuizDemoClasses/QuizManagement/TestPaper.cs
@@ -0,0 +1,17 @@
+﻿namespace QuizDemoClasses.QuizManagement
+{
+    public class TestPaper
+    {
+        public int Id { get; set; }
+        public int RegistrartionId { get; set; }
+        public int TestQuestionId { get; set; }
+        public int ChoiceId { get; set; }
+        public string Answer { get; set; }
+        public string MarkScored { get; set; }
+
+        public Choice Choice { get; set; }
+
+
+
+    }
+}
