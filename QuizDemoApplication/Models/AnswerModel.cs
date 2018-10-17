ff11d167272940922ff1f65ed9d4dacdc317b58b
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2018-10-18 00:33:53 +0500

    Update Added Choice Model in the Model class

---
+++ QuizDemoApplication/Models/AnswerModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QuizDemoApplication.Models
+{
+    public class AnswerModel
+    {
+        public int TestId { get; set; }
+        public int QuestionId { get; set; }
+        public Guid Token { get; set; }
+        public List<ChoiceModel> ChoiceModels { get; set; }
+
+        public string Answer { get; set; }
+        public string Direction { get; set; }
+
+        public List<int> UserSelectedId => ChoiceModels == null ? new List<int>() :
+                     (List<int>)ChoiceModels.Where(x =>
+                        x.IsChecked == "on" || "true".Equals(x.IsChecked, StringComparison.CurrentCultureIgnoreCase));
+    }
+}
